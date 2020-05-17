c70ae887294502e5152cc7971ff85abd2791cf73
Shivanshu Tyagi <shivanshutyagi3@gmail.com>
2020-05-17 23:46:05 +0530

    script to convert video to frame

---
+++ change.sh
@@ -0,0 +1,10 @@
+avi=`find ${PATH}/charades/Charades_v1_480 -name \*.mp4`
+
+for i in $avi; do
+  dir=`echo $i | cut -d. -f1`
+  f1=`echo $dir | cut -d/ -f7`
+  echo $dir $f1
+  rm -rf ./frames/$f1/
+  mkdir -p ./frames/$f1/
+  ffmpeg -i $i -q:v 1 -r 25 ./frames/$f1/image_%5d.jpg
+done
