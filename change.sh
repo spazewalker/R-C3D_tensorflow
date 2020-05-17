36963b7ff3709fe7b1542014353e11e270de312f
Shivanshu Tyagi <shivanshutyagi3@gmail.com>
2020-05-18 00:32:11 +0530

    Update change.sh

--- change.sh
+++ change.sh
@@ -1,10 +1,8 @@
-avi=`find /charades/Charades_v1_480 -name \*.mp4`
-
-for i in $avi; do
-  dir=`echo $i | cut -d. -f1`
-  f1=`echo $dir | cut -d/ -f7`
-  echo $dir $f1
-  rm -rf ./frames/$f1/
-  mkdir -p ./frames/$f1/
-  ffmpeg -i $i -q:v 1 -r 25 ./frames/$f1/image_%5d.jpg
+FILES=./charades/Charades_v1_480/*mp4
+for f in $FILES
+do
+  dir=$(echo $f | sed 's/.mp4//g' | sed 's/.\/charades\/Charades_v1_480\///g')
+  mkdir -p ./frames/$dir/
+  # echo $dir
+  ffmpeg -i $f -q:v 1 -r 25 ./frames/$dir/image_%5d.jpg
 done
